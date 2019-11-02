174435997f6571979a92e8db40fe66c9fbc93d14
mayb <mayb@rp-pet.cn>
2019-11-02 12:01:58 +0800

    update

--- golog/grpc.go
+++ golog/grpc.go
@@ -3,30 +3,24 @@ package golog
 import (
 	"context"
 	"fmt"
-	"github.com/limitedlee/microservice/config"
-	"github.com/limitedlee/microservice/model"
-	"github.com/limitedlee/microservice/proto"
-	"google.golang.org/grpc"
 	"log"
 	"runtime"
 	"time"
+
+	"github.com/limitedlee/microservice/config"
+	"github.com/limitedlee/microservice/model"
+	"google.golang.org/grpc"
 )
 
-var client proto.LogClient
+var logGrpcUrl string
 
 func init() {
 	//获取项目配置中的数据
-	logGrpcUrl, err := config.Get("LogGrpc")
+	var err error
+	logGrpcUrl, err = config.Get("LogGrpc")
 	if err != nil {
-		log.Fatalf("get config info fail: %v", err)
+		log.Printf("get config info fail: %v", err)
 	}
-
-	conn, err := grpc.Dial(logGrpcUrl, grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	//defer conn.Close()
-	client = proto.NewLogClient(conn)
 }
 func Error(in ...interface{}) {
 	defer func() {
@@ -89,48 +83,62 @@ func Warn(in ...interface{}) {
 	writeLog(fmt.Sprintf("%v => %v", f.Name(), f2.Name()), "WARN", in)
 }
 
-func writeLog(logger string, level string, in []interface{}) {
-	go func() {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Println(err)
+func writeLog(logger string, level string, in []interface{}) (r *Reply) {
+	for {
+		err := func() (err error) {
+			defer func() {
+				if err := recover(); err != nil {
+					log.Println(err)
+				}
+			}()
+
+			var conn *grpc.ClientConn
+			conn, err = grpc.Dial(logGrpcUrl, grpc.WithInsecure())
+			if err != nil {
+				return err
 			}
+			defer conn.Close()
+			client := NewLogClient(conn)
+
+			ctx, cf := context.WithTimeout(context.Background(), time.Second*60)
+			defer cf()
+
+			m := LogRequest{}
+			m.Logger = logger
+			m.Appid = model.PbConfig.Grpc.Appid
+
+			for _, v := range in {
+				switch v.(type) {
+				case string:
+					m.Message += v.(string)
+				case runtime.Error:
+					m.Exception = v.(runtime.Error).Error()
+				case error:
+					m.Exception = v.(error).Error()
+				default:
+					m.Exception = fmt.Sprintf("%T - ", v) + fmt.Sprintf("%v", v)
+				}
+			}
+
+			switch level {
+			case "ERROR":
+				r, err = client.Error(ctx, &m)
+			case "INFO":
+				r, err = client.Info(ctx, &m)
+			case "WARN":
+				r, err = client.Warn(ctx, &m)
+			case "FATAL":
+				r, err = client.Fatal(ctx, &m)
+			}
+			return
 		}()
 
-		m := proto.LogRequest{}
-		m.Logger = logger
-		m.Appid = model.PbConfig.Grpc.Appid
-
-		for _, v := range in {
-			switch v.(type) {
-			case string:
-				m.Message += v.(string)
-			case runtime.Error:
-				m.Exception = v.(runtime.Error).Error()
-			case error:
-				m.Exception = v.(error).Error()
-			default:
-				m.Exception = fmt.Sprintf("%T - ", v) + fmt.Sprintf("%v", v)
-			}
+		if err != nil {
+			log.Printf("%v\n", err)
+			time.Sleep(time.Second * 3)
+		} else {
+			break
 		}
-
-		ctx, _ := context.WithTimeout(context.Background(), time.Second*60)
-
-		var r *proto.Reply
-		var err2 error
-		switch level {
-		case "ERROR":
-			r, err2 = client.Error(ctx, &m)
-		case "INFO":
-			r, err2 = client.Info(ctx, &m)
-		case "WARN":
-			r, err2 = client.Warn(ctx, &m)
-		case "FATAL":
-			r, err2 = client.Fatal(ctx, &m)
-		}
-		if err2 != nil {
-			log.Fatalf("could not greet: %v", err2)
-		}
-		log.Printf("[gRPC-LogService] %v\t%+v",r,m)
-	}()
+	}
+	return r
 }
